7b3edb871740faece63d4f36e5277a4fe5e5d8b9
vijaycrzzy <101950044+vijaycrzzy@users.noreply.github.com>
2023-09-08 14:46:49 +0530

    Update account.js

--- account.js
+++ account.js
@@ -1,24 +1,69 @@
+// Login , Signup Page
 
 function login()
 {
  var log= document.getElementById("login");
 log.style.color="grey";
-var reg=document.getElementById("register");
-reg.style.color="black";
-document.getElementById("email").style.display="none";
-document.getElementById("reg").innerHTML="Login";
+var register=document.getElementById("register");
+register.style.color="black";
+var loginbox=document.getElementById("loginbox");
+var registerbox=document.getElementById("registerbox");
+if(loginbox.style.display==="none")
+{
+   loginbox.style.display="flex";
+   registerbox.style.display="none";
+}
 }
 function register()
 {
- var link=document.getElementById("reg");
- if(document.getElementById("user").value=="vijay")
- {
-    link.href="file:///C:/Users/vijay/OneDrive/Desktop/RESUME%20WEBSITE/Ecommerce%20Website/ecom.html";
- }
- var reg=document.getElementById("register");
- reg.style.color="grey";
+ var loginbox=document.getElementById("loginbox");
+var registerbox=document.getElementById("registerbox");
+ var register=document.getElementById("register");
+ register.style.color="grey";
  var log= document.getElementById("login");
 log.style.color="black";
-document.getElementById("email").style.display="block";
-document.getElementById("reg").innerHTML="Register";
+if(registerbox.style.display==="none")
+   {
+      registerbox.style.display="flex";
+      loginbox.style.display="none";
+   }
+}
+let users=[];
+let passwords=[];
+function Register()
+{
+   var user1=document.getElementById("user1").value;
+   var password1=document.getElementById("password1").value;
+   if(users.includes(user1))
+   {
+      alert("user is taken");
+   }
+   else{
+      alert("Successfully Registered");
+      users.push(user1);
+      passwords.push(password1);
+   }
+}
+function Login()
+{
+   var reg=document.getElementById("reg");
+   var user1=document.getElementById("user").value;
+   var password1=document.getElementById("password").value;
+   if(users.includes(user1))
+   {
+      for(let i=0;i<users.length;i++)
+      {
+        if(users[i]===user1 && passwords[i]===password1)
+        {
+         reg.href="file:///C:/Users/vijay/OneDrive/Desktop/Arduino/redstore%20ecom/ecom.html";
+        }
+        else
+        {
+         alert("password incorrect");
+        }
+      }
+   }
+   else{
+      alert("user does not exist");
+   }
 }
